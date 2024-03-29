9f03acc5384228eda6954efd916a45d8b37dfd26
Seren Kwok <seren@grit.io>
2024-03-29 14:35:46 -0700

    some border

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-sans text-sm lg:flex flex-col">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-sans text-sm lg:flex flex-col gap-12">
         <div className="flex-col items-start justify-between w-full">
           <label
             htmlFor="message"
@@ -15,6 +15,19 @@ export default function Home() {
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Write your thoughts here..."
           />
+        </div>
+        <div className="flex-col items-start justify-between w-full">
+          <label
+            htmlFor="message"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Translated text
+          </label>
+          <div className="rounded-lg w-full gradient-border">
+            <div className='relative z-10 flex items-center gap-1.5 px-3 py-3'>
+            WTF
+            </div>
+          </div>
         </div>
       </div>
     </main>
