0bf373533a5f79dad6f5cb8a3164052fc694d9f5
Seren Kwok <seren@grit.io>
2024-03-29 12:52:58 -0700

    yolo

--- similarity.py
+++ similarity.py
@@ -19,6 +19,17 @@ def main():
     print("Cosine similarity:", cosine_scores.item())
 
 
+def compute_similarity(sentence1: str, sentence2: str) -> float:
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    embeddings1 = model.encode(sentence1, convert_to_tensor=True)
+    embeddings2 = model.encode(sentence2, convert_to_tensor=True)
+
+    cosine_scores = util.pytorch_cos_sim(embeddings1, embeddings2)
+
+    return cosine_scores.item()
+
+
 # if directly invoking as __main__ then call the function main
 if __name__ == "__main__":
     main()
