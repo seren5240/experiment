a338358f1c580b6e67d0c2113c797570bfb1261d
Seren Kwok <seren@grit.io>
2024-03-30 18:01:09 -0700

    heres my number

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -62,25 +62,42 @@ export default function Home() {
               htmlFor="message"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Original text
+              What would you like to translate?
             </label>
             <textarea
               id="message"
               rows={4}
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Write your thoughts here..."
+              placeholder="Write your input here..."
               ref={inputRef}
+              disabled={loading}
             />
+            {error !== undefined && (
+              <div
+                className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-3"
+                role="alert"
+              >
+                <strong className="font-bold">Error:{' '}</strong>
+                <span className="block sm:inline">{error}</span>
+              </div>
+            )}
+          </div>
+          <div className="flex-col items-start justify-between w-full">
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Original text
+            </label>
+            {(loading || response) && (
+              <div className="rounded-lg w-full gradient-border">
+                <div className="relative z-10 flex items-center gap-1.5 px-3 py-3">
+                  {response ? response.original : inputRef.current?.value}
+                </div>
+              </div>
+            )}
           </div>
           <div className="flex-col items-start justify-between w-full">
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
               Translated text
             </label>
-            {error !== undefined && (
-              <div className="rounded-lg w-full bg-red-600 p-3 text-white">
-                {error}
-              </div>
-            )}
             {response !== undefined && (
               <div className="flex flex-col items-start justify-between w-full gap-2">
                 <div className="rounded-lg w-full gradient-border">
