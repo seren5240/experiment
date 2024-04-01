f95c2dfcc8195d78abafc0df5c621ca8bbf9210c
Seren Kwok <seren@grit.io>
2024-03-31 20:09:06 -0700

    impossible tests

--- server/model/explanation.py
+++ server/model/explanation.py
@@ -1,8 +1,9 @@
 import aiounittest
 import uuid
+from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from model.base import StepExplanation
+from model.base import StepExplanation, Translation
 from sqlalchemy.future import select
 
 from model.db import get_managed_db_session
@@ -28,3 +29,40 @@ class TestLanguageMethods(aiounittest.AsyncTestCase):
                 random_id, 0, session
             )
             self.assertIsNone(explanation)
+
+    async def test_gets_existing_explanation(self):
+        translation_id = str(uuid.uuid4())
+        step_index = 0
+        explanation_text = "Test explanation"
+        async with get_managed_db_session() as session:
+            session.add(
+                Translation(
+                    id=translation_id,
+                    original="Original placeholder",
+                    final="Final placeholder",
+                    steps=[{"text": "Test step", "language": "en"}],
+                    similarity=0.5,
+                ),
+            )
+            await session.commit()
+            session.add(
+                StepExplanation(
+                    id=str(uuid.uuid4()),
+                    translation_id=translation_id,
+                    step_index=step_index,
+                    explanation=explanation_text,
+                ),
+            )
+            await session.commit()
+            fetched = await fetch_by_translation_id_step_index(
+                translation_id, step_index, session
+            )
+            self.assertIsNotNone(fetched)
+            self.assertEqual(fetched.explanation, explanation_text)
+            query = delete(StepExplanation).filter(
+                StepExplanation.translation_id == translation_id
+            )
+            await session.execute(query)
+            query = delete(Translation).filter(Translation.id == translation_id)
+            await session.execute(query)
+            await session.commit()
