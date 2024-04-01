d89294aae9635a5ba0d0ab7ed91d74b2fe10e8ab
Seren Kwok <seren@grit.io>
2024-03-31 21:28:16 -0700

    unimportant

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
       },
       body: JSON.stringify({
         text: input,
-        target_languages: [...languages.map((l) => l.code), 'en'],
+        target_languages: [...languages.map((l) => l.code), "en"],
       }),
     });
     const data: TranslationResponse = await res.json();
@@ -58,82 +58,84 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="max-w-5xl w-full flex gap-12 items-start">
-        <div className="z-10 max-w-5xl w-full items-start justify-between font-sans text-sm lg:flex flex-col gap-12 flex">
-          <p>
-            This is a tool to see the semantic similarity of English phrases
-            after bounced through multiple languages using Microsoft&apos;s
-            Azure Translator API. The final transformation, after translating
-            through all selected languages, will always be back to English, so
-            it is not necessary to select English as the final language on the
-            right sidebar. For example, if you select French, English, and
-            Spanish in that order, then the full order of translation will go
-            English &gt; French &gt; English &gt; Spanish &gt; English, with the
-            final English output displayed under &quot;Final text&quot;.
-          </p>
-          <div className="flex-col items-start justify-between w-full">
-            <label
-              htmlFor="message"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              What would you like to translate?
-            </label>
-            <textarea
-              id="message"
-              rows={4}
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Write your input here..."
-              ref={inputRef}
-              disabled={loading || loadingStored}
-            />
-            {error !== undefined && (
-              <div
-                className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-3"
-                role="alert"
+      <div className="max-w-5xl w-full flex flex-col gap-12 items-start">
+        <p className="text-sm">
+          This is a tool to see the semantic similarity of English phrases after
+          bounced through multiple languages using Microsoft&apos;s Azure
+          Translator API. The final transformation, after translating through
+          all selected languages, will always be back to English, so it is not
+          necessary to select English as the final language on the right
+          sidebar. For example, if you select French, English, and Spanish in
+          that order, then the full order of translation will go English &gt;
+          French &gt; English &gt; Spanish &gt; English, with the final English
+          output displayed under &quot;Final text&quot;.
+        </p>
+        <div className="max-w-5xl w-full flex gap-12 items-start items-start">
+          <div className="z-10 max-w-5xl w-full items-start justify-between font-sans text-sm lg:flex flex-col gap-12 flex">
+            <div className="flex-col items-start justify-between w-full">
+              <label
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
-                <strong className="font-bold">Error: </strong>
-                <span className="block sm:inline">{error}</span>
-              </div>
-            )}
-          </div>
-          <div className="flex-col items-start justify-between w-full">
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-              Original text
-            </label>
-            {(loading || response) && (
-              <div className="rounded-lg w-full gradient-border">
-                <div className="relative z-10 flex items-center gap-1.5 px-3 py-3">
-                  {response ? response.original : inputRef.current?.value}
+                What would you like to translate?
+              </label>
+              <textarea
+                id="message"
+                rows={4}
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Write your input here..."
+                ref={inputRef}
+                disabled={loading || loadingStored}
+              />
+              {error !== undefined && (
+                <div
+                  className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-3"
+                  role="alert"
+                >
+                  <strong className="font-bold">Error: </strong>
+                  <span className="block sm:inline">{error}</span>
                 </div>
-              </div>
-            )}
-          </div>
-          <div className="flex-col items-start justify-between w-full">
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-              Final text
-            </label>
-            {response !== undefined && (
-              <div className="flex flex-col items-start justify-between w-full gap-2">
+              )}
+            </div>
+            <div className="flex-col items-start justify-between w-full">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Original text
+              </label>
+              {(loading || response) && (
                 <div className="rounded-lg w-full gradient-border">
                   <div className="relative z-10 flex items-center gap-1.5 px-3 py-3">
-                    {response.final}
+                    {response ? response.original : inputRef.current?.value}
                   </div>
                 </div>
-                <p className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Similarity: {response.similarity.toFixed(4)}
-                </p>
-              </div>
-            )}
+              )}
+            </div>
+            <div className="flex-col items-start justify-between w-full">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Final text
+              </label>
+              {response !== undefined && (
+                <div className="flex flex-col items-start justify-between w-full gap-2">
+                  <div className="rounded-lg w-full gradient-border">
+                    <div className="relative z-10 flex items-center gap-1.5 px-3 py-3">
+                      {response.final}
+                    </div>
+                  </div>
+                  <p className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Similarity: {response.similarity.toFixed(4)}
+                  </p>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-        <div
-          className="flex flex-col h-full gap-8 items-center"
-          style={{ minWidth: "30%" }}
-        >
-          <Button onClick={translateText} disabled={loading || loadingStored}>
-            Translate
-          </Button>
-          <Languages languages={languages} setLanguages={setLanguages} />
+          <div
+            className="flex flex-col h-full gap-8 items-center"
+            style={{ minWidth: "30%" }}
+          >
+            <Button onClick={translateText} disabled={loading || loadingStored}>
+              Translate
+            </Button>
+            <Languages languages={languages} setLanguages={setLanguages} />
+          </div>
         </div>
       </div>
     </main>
