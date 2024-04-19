54cd123d97a03cc90702eb3c211f64842b88b2ac
Seren Kwok <seren@grit.io>
2024-04-19 14:22:31 +0100

    some summary

--- server/scripts/summarize.py
+++ server/scripts/summarize.py
@@ -1,3 +1,4 @@
+import os
 from bs4 import BeautifulSoup
 import aiohttp
 import asyncio
@@ -10,7 +11,34 @@ async def main():
         response = await session.get(url)
         text = await response.text()
         soup = BeautifulSoup(text, "html.parser")
-        print(soup.get_text())
+        text = soup.get_text()
+
+        api_key = os.getenv("OPENROUTER_API_KEY")
+        payload = {
+            "model": "mistralai/mistral-7b-instruct:free",
+            "messages": [
+                {
+                    "role": "system",
+                    "content": """You are an expert analyst tasked with summarizing a news article whose text, extracted from the HTML of its webpage, is provided below.
+                    Please return a paragraph-long summary of the article. Ignore any extraneous text that may have been extracted from the HTML which is not related to the article.""",
+                },
+                {
+                    "role": "user",
+                    "content": f"""Article text:
+    {text}""",
+                },
+            ],
+        }
+        headers = {
+            "Authorization": f"Bearer {api_key}",
+        }
+        response = await session.post(
+            url="https://openrouter.ai/api/v1/chat/completions",
+            json=payload,
+            headers=headers,
+        )
+        data = await response.json()
+        print(data["choices"][0]["message"]["content"])
 
 
 asyncio.run(main())
