5a166d4681701facf4c27a486071a9d5496983ac
Seren Kwok <seren@grit.io>
2024-04-24 17:24:49 +0100

    try just this

--- server/model/score.py
+++ server/model/score.py
@@ -6,8 +6,14 @@ from model.base import Score
 
 
 async def update_leaderboard(session: AsyncSession, score: Score) -> Sequence[Score]:
+    article_id = score.article_id
     session.add(score)
     await session.commit()
-    query = select(Score).order_by(Score.score.desc()).limit(10)
+    query = (
+        select(Score)
+        .where(Score.article_id == article_id)
+        .order_by(Score.score.desc())
+        .limit(10)
+    )
     res = await session.execute(query)
     return res.scalars().all()
